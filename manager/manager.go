f3656069c851f984ba47f32b2f605b9bb14a6d74
paddlesteamer <umuter@protonmail.com>
2020-06-18 14:57:31 +0300

    initial sync

--- manager/manager.go
+++ manager/manager.go
@@ -58,38 +58,49 @@ func NewManager(conf *config.Configuration) (*Manager, error) {
 	if err != nil {
 		return nil, fmt.Errorf("manager: unable to create database file: %v", err)
 	}
-	// defer dbf.Close() close manually, shouldn't be deferred
+	// defer dbf.Close() close manually, at least shouldn't be deferred here
 
-	dbPath := fmt.Sprintf("/%s", u.Host)
+	dbExtPath := fmt.Sprintf("/%s", u.Host)
+	dbPath := dbf.Name()
 
 	_, dbr, err := drv.GetFile(u.Host)
 	if err != nil { // TODO: check specific 'not found' error
 		// below is for not found error
 		dbf.Close()
 
-		err = db.InitDB(dbf.Name())
+		err = db.InitDB(dbPath)
 		if err != nil {
 			return nil, fmt.Errorf("manager: unable to initialize db: %v", err)
 		}
 
-		// TODO: upload db
+		dbf, err := os.Open(dbPath)
+		if err != nil {
+			os.Remove(dbPath)
+			return nil, fmt.Errorf("manager: unable to open intitialized db: %v", err)
+		}
+		defer dbf.Close()
+
+		err = drv.PutFile(dbExtPath, dbf)
+		if err != nil {
+			os.Remove(dbPath)
+			return nil, fmt.Errorf("manager: unable to upload initialiezed db: %v", err)
+		}
 
 	} else {
 		defer dbr.Close()
+		defer dbf.Close()
 
 		_, err := io.Copy(dbf, dbr)
 		if err != nil {
 			return nil, fmt.Errorf("manager: unable to copy contents of db to local file: %v", err)
 		}
-
-		dbf.Close()
 	}
 
 	db := dbconn{
 		extDrive:    drv,
-		extFilePath: dbPath,
+		extFilePath: dbExtPath,
 
-		dbPath:      dbf.Name(),
+		dbPath:      dbPath,
 	}
 
 	m := &Manager{
